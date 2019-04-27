d67e51f451f5bcd98f0558a9e2ab4dce7f1f3b20
diegomtassis <diego.mt.dev@gmail.com>
2019-04-27 21:02:52 +0200

    * enemies crash into platforms

--- src/enemies.c
+++ src/enemies.c
@@ -14,7 +14,7 @@
 #include "../res/sprite.h"
 
 #define SPEED_ZERO		FIX16(0)
-#define SPEED_H_NORMAL	FIX16(1.5)
+#define SPEED_H_NORMAL	FIX16(1.1)
 #define SPEED_V_NORMAL	FIX16(0.2)
 
 #define ENEMY_HEIGHT	16
@@ -23,20 +23,25 @@
 #define MIN_POS_H_PX_F16	LEFT_POS_H_PX_F16 - FIX16(-8)
 #define MAX_POS_H_PX_F16	RIGHT_POS_H_PX_F16 - FIX16(8)
 #define MIN_POS_V_PX_F16	TOP_POS_V_PX_F16
-#define MAX_POS_V_PX_F16	BOTTOM_POS_V_PX_F16 - FIX16(8)
+#define MAX_POS_V_PX_F16	BOTTOM_POS_V_PX_F16 - FIX16(16)
 
 static Enemy* createEnemy();
 static void moveEnemy(Enemy*, Sprite*, const Level*);
 static void calculateNextMovement(Enemy*);
 static void updatePosition(Enemy*, const Level*);
+static fix16 crashedHIntoPlatform(Box_f16 subject_box, const Level* level);
+static fix16 crashedVIntoPlatform(Box_f16 subject_box, const Level* level);
+static void updateSprite(Enemy* enemy, Sprite* sprite);
 
-static fix16 crashedV(Box_f16 subject_box, const Level* level);
+static void detectExplosion();
 
 Sprite** enemiesSprites;
 
 Enemy** enemies;
 
 u32 lastCreatedEnemy;
+
+u8 explode_all;
 
 void startEnemies(Level* level) {
 
@@ -66,11 +71,23 @@ void startEnemies(Level* level) {
 
 void handleEnemies(Level* level) {
 
+	detectExplosion();
+
 	u8 num_enemies = level->enemies->max_num_enemies;
+
 	while (num_enemies--) {
+
 		Enemy* enemy = enemies[num_enemies];
 		if (enemy) {
-			moveEnemy(enemy, enemiesSprites[num_enemies], level);
+
+			Sprite* sprite = enemiesSprites[num_enemies];
+			if (explode_all) {
+				enemy->alive = FALSE;
+			} else {
+				moveEnemy(enemy, sprite, level);
+			}
+
+			updateSprite(enemy, sprite);
 
 			// get rid of dead bodies
 			if (!enemy->alive) {
@@ -80,46 +97,8 @@ void handleEnemies(Level* level) {
 			}
 		}
 	}
-}
 
-static void moveEnemy(Enemy* enemy, Sprite* sprite, const Level* level) {
-
-	calculateNextMovement(enemy);
-	updatePosition(enemy, level);
-
-	if (enemy->alive) {
-		SPR_setPosition(sprite, fix16ToInt(enemy->object.pos.x), fix16ToInt(enemy->object.pos.y));
-	} else {
-		SPR_releaseSprite(sprite);
-	}
-}
-
-static void calculateNextMovement(Enemy* enemy) {
-	// nothing for the moment
-}
-
-static void updatePosition(Enemy* enemy, const Level* level) {
-
-	// horizontal position
-	Box_f16 target_h = targetHBox(&enemy->object, ENEMY_WIDTH, ENEMY_HEIGHT);
-	if (target_h.x > MAX_POS_H_PX_F16) {
-		enemy->object.pos.x = MIN_POS_H_PX_F16;
-
-	} else if (target_h.x < MIN_POS_H_PX_F16) {
-		enemy->object.pos.x = MAX_POS_H_PX_F16;
-
-	} else {
-		enemy->object.pos.x = target_h.x;
-	}
-
-	// vertical position
-	Box_f16 target_v = targetVBox(&enemy->object, ENEMY_WIDTH, ENEMY_HEIGHT);
-	fix16 crashed = crashedV(target_v, level);
-	if (crashed) {
-		enemy->alive = FALSE;
-	} else {
-		enemy->object.pos.y = target_v.y;
-	}
+	explode_all = FALSE;
 }
 
 static Enemy* createEnemy() {
@@ -149,7 +128,57 @@ static Enemy* createEnemy() {
 	return enemy;
 }
 
-static fix16 crashedV(Box_f16 subject_box, const Level* level) {
+static void moveEnemy(Enemy* enemy, Sprite* sprite, const Level* level) {
+
+	calculateNextMovement(enemy);
+	updatePosition(enemy, level);
+}
+
+static void calculateNextMovement(Enemy* enemy) {
+	// nothing for the moment
+}
+
+static void updatePosition(Enemy* enemy, const Level* level) {
+
+	// horizontal position
+	Box_f16 target_h = targetHBox(&enemy->object, ENEMY_WIDTH, ENEMY_HEIGHT);
+	if (target_h.x > MAX_POS_H_PX_F16) {
+		enemy->object.pos.x = MIN_POS_H_PX_F16;
+
+	} else if (target_h.x < MIN_POS_H_PX_F16) {
+		enemy->object.pos.x = MAX_POS_H_PX_F16;
+
+	} else if (crashedHIntoPlatform(target_h, level)) {
+		enemy->alive = FALSE;
+	} else {
+		enemy->object.pos.x = target_h.x;
+	}
+
+	// vertical position
+	Box_f16 target_v = targetVBox(&enemy->object, ENEMY_WIDTH, ENEMY_HEIGHT);
+	if (crashedVIntoPlatform(target_v, level)) {
+		enemy->alive = FALSE;
+	} else {
+		enemy->object.pos.y = target_v.y;
+	}
+}
+
+static fix16 crashedHIntoPlatform(Box_f16 subject_box, const Level* level) {
+
+	fix16 crashed;
+	for (u8 i = 0; i < level->num_platforms; i++) {
+		Box_f16 platform_box = *level->platforms[i]->object.box;
+		crashed = hitLeft(subject_box, platform_box) //
+		|| hitRight(subject_box, platform_box);
+		if (crashed) {
+			return crashed;
+		}
+	}
+
+	return 0;
+}
+
+static fix16 crashedVIntoPlatform(Box_f16 subject_box, const Level* level) {
 
 	fix16 crashed = hitAbove(subject_box, *level->floor->object.box);
 	if (crashed) {
@@ -157,11 +186,29 @@ static fix16 crashedV(Box_f16 subject_box, const Level* level) {
 	}
 
 	for (u8 i = 0; i < level->num_platforms; i++) {
-		crashed = hitAbove(subject_box, *level->platforms[i]->object.box);
+		Box_f16 platform_box = *level->platforms[i]->object.box;
+		crashed = hitAbove(subject_box, platform_box) //
+		|| hitUnder(subject_box, platform_box);
 		if (crashed) {
 			return crashed;
 		}
 	}
 
 	return 0;
+}
+
+static void updateSprite(Enemy* enemy, Sprite* sprite) {
+
+	if (enemy->alive) {
+		SPR_setPosition(sprite, fix16ToInt(enemy->object.pos.x), fix16ToInt(enemy->object.pos.y));
+	} else {
+		SPR_releaseSprite(sprite);
+	}
+}
+
+static void detectExplosion() {
+
+	if (JOY_readJoypad(JOY_1) & BUTTON_A) {
+		explode_all = TRUE;
+	}
 }
