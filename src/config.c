fed49b43efaaf4d1d3bb60d69a0dcdc72cffb5bb
diegomtassis <diego.mt.dev@gmail.com>
2019-05-23 23:10:38 +0200

    * MD planet 03

--- src/config.c
+++ src/config.c
@@ -13,6 +13,7 @@
 #include "../inc/planets/level_zx_02.h"
 #include "../inc/planets/level_md_01.h"
 #include "../inc/planets/level_md_02.h"
+#include "../inc/planets/level_md_03.h"
 
 #define NUM_MODES		2
 
@@ -110,10 +111,11 @@ static void setGameConfig(Game* game) {
 		game->createLevel[1] = createLevelZX02;
 	} else {
 		game->mode = MODE_MD;
-		game->num_levels = 2;
+		game->num_levels = 3;
 		game->createLevel = MEM_alloc(game->num_levels * sizeof(Level*));
 		game->createLevel[0] = createLevelMD01;
 		game->createLevel[1] = createLevelMD02;
+		game->createLevel[2] = createLevelMD03;
 	}
 
 	game->lives = 3;
--- src/planets/level_md_03.c
+++ src/planets/level_md_03.c
@@ -1,11 +1,11 @@
 /*
- * level_md_02.c
+ * level_md_03.c
  *
- *  Created on: May 12, 2019
+ *  Created on: May 23, 2019
  *      Author: diegomtassis
  */
 
-#include "../../inc/planets/level_md_02.h"
+#include "../../inc/planets/level_md_03.h"
 
 #include <genesis.h>
 
@@ -17,7 +17,7 @@ static void createPlatforms(Level* level);
 static void defineEnemies(Enemies* enemies);
 static void defineSpaceship(Level* level);
 
-Level* createLevelMD02() {
+Level* createLevelMD03() {
 
 	Level* level = allocLevel();
 
@@ -26,7 +26,7 @@ Level* createLevelMD02() {
 	defineSpaceship(level);
 
 	level->def.jetman_init_pos = MEM_alloc(sizeof *level->def.jetman_init_pos);
-	setV2s16(level->def.jetman_init_pos, 88, 72);
+	setV2s16(level->def.jetman_init_pos, 68, 176);
 
 	level->def.mind_bottom = TRUE;
 
@@ -35,10 +35,15 @@ Level* createLevelMD02() {
 
 static void createPlatforms(Level* level) {
 
-	level->floor = createPlatform(14, 18, 5);
+	level->floor = createPlatform(1, 22, 11);
 
-	level->num_platforms = 0;
+	level->num_platforms = 4;
 	level->platforms = MEM_alloc(level->num_platforms * sizeof(Platform*));
+
+	level->platforms[0] = createPlatform(0, 7, 4);
+	level->platforms[1] = createPlatform(8, 7, 2);
+	level->platforms[2] = createPlatform(9, 10, 22);
+	level->platforms[3] = createPlatform(30, 7, 2);
 }
 
 static void defineEnemies(Enemies* enemies) {
@@ -51,5 +56,5 @@ static void defineSpaceship(Level* level) {
 
 	level->def.spaceship_def.type = U1;
 	level->def.spaceship_def.init_step = ASSEMBLED;
-	setV2s16(&level->def.spaceship_def.base_pos, 124, 96);
+	setV2s16(&level->def.spaceship_def.base_pos, 40, 96);
 }
