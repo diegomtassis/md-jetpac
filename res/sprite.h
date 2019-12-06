5e6993ec6eb9ba0bb6dcfe66735bd4bbc1b6e075
diegomtassis <diego.mt.dev@gmail.com>
2019-12-06 17:59:54 +0100

    * u2 and u3 sprites

--- res/sprite.h
+++ res/sprite.h
@@ -2,6 +2,20 @@
 #define _RES_SPRITE_H_
 
 extern const SpriteDefinition jetman_sprite;
+extern const SpriteDefinition shot_sprite;
+extern const SpriteDefinition u1_base_sprite;
+extern const SpriteDefinition u1_middle_sprite;
+extern const SpriteDefinition u1_top_sprite;
+extern const SpriteDefinition u1_sprite;
+extern const SpriteDefinition u2_base_sprite;
+extern const SpriteDefinition u2_middle_sprite;
+extern const SpriteDefinition u2_top_sprite;
+extern const SpriteDefinition u2_sprite;
+extern const SpriteDefinition u3_base_sprite;
+extern const SpriteDefinition u3_middle_sprite;
+extern const SpriteDefinition u3_top_sprite;
+extern const SpriteDefinition u3_sprite;
+extern const SpriteDefinition fire_sprite;
 extern const SpriteDefinition meteorite_sprite;
 extern const SpriteDefinition alien_sprite;
 extern const SpriteDefinition bubble_sprite;
@@ -10,19 +24,13 @@ extern const SpriteDefinition saucer_sprite;
 extern const SpriteDefinition falcon_sprite;
 extern const SpriteDefinition cross_sprite;
 extern const SpriteDefinition owl_sprite;
-extern const SpriteDefinition u1_base_sprite;
-extern const SpriteDefinition u1_middle_sprite;
-extern const SpriteDefinition u1_top_sprite;
-extern const SpriteDefinition u1_sprite;
+extern const SpriteDefinition boom_sprite;
 extern const SpriteDefinition fuel_sprite;
 extern const SpriteDefinition gold_sprite;
 extern const SpriteDefinition nuke_sprite;
 extern const SpriteDefinition triatom_sprite;
 extern const SpriteDefinition bean_sprite;
 extern const SpriteDefinition diamond_sprite;
-extern const SpriteDefinition boom_sprite;
 extern const SpriteDefinition cursor_sprite;
-extern const SpriteDefinition fire_sprite;
-extern const SpriteDefinition shot_sprite;
 
 #endif // _RES_SPRITE_H_
